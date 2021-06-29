61b5f66c749743d5f5cfce9e278f2b1e3489e4af
Calvindes <calvindes90@gmail.com>
2021-06-29 23:22:57 +0100

    base structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Game/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Game from './components/Game.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Game
   }
 }
 </script>
@@ -22,5 +22,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img{
+
+    max-width: 150px;
+    max-height: 150px;
+    border-radius: 200px;
+    box-shadow: 0px 0px 25px rgba(0,0,0,0.5);
+
 }
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="hello">
+    <h1>Cuatro en Raya</h1>
+    <p>
+      ¡Lucha contra la máquina!
+    </p>
+    
+      <div class="main">
+          <div class="container">
+              <b-row class="row-y1">
+                  <b-col class="col-y1">
+                      <div class="circle" data-y="1" data-x="1"></div>
+                  </b-col>
+                  <b-col class="col-y1">
+                      <div class="circle" data-y="1" data-x="2"></div>
+                  </b-col>
+                  <b-col class="col-y1">
+                      <div class="circle" data-y="1" data-x="3"></div>
+                  </b-col>
+                  <b-col class="col-y1">
+                      <div class="circle" data-y="1" data-x="4"></div>
+                  </b-col>
+              </b-row>
+              <b-row class="row-y2">
+                  <b-col class="col-y2">
+                      <div class="circle" data-y="2" data-x="1"></div>
+                  </b-col>
+                  <b-col class="col-y2">
+                      <div class="circle" data-y="2" data-x="2"></div>
+                  </b-col>
+                  <b-col class="col-y2">
+                      <div class="circle" data-y="2" data-x="3"></div>
+                  </b-col>
+                  <b-col class="col-y2">
+                      <div class="circle" data-y="2" data-x="4"></div>
+                  </b-col>
+              </b-row>
+              <b-row class="row-y3">
+                  <b-col class="col-y3">
+                      <div class="circle" data-y="3" data-x="1"></div>
+                  </b-col>
+                  <b-col class="col-y3">
+                      <div class="circle" data-y="3" data-x="2"></div>
+                  </b-col>
+                  <b-col class="col-y3">
+                      <div class="circle" data-y="3" data-x="3"></div>
+                  </b-col>
+                  <b-col class="col-y3">
+                      <div class="circle" data-y="3" data-x="4"></div>
+                  </b-col>
+              </b-row>
+              <b-row class="row-y4">
+                  <b-col class="col-y4">
+                      <div class="circle" data-y="4" data-x="1"></div>
+                  </b-col>
+                  <b-col class="col-y4">
+                      <div class="circle" data-y="4" data-x="2"></div>
+                  </b-col>
+                  <b-col class="col-y4">
+                      <div class="circle" data-y="4" data-x="3"></div>
+                  </b-col>
+                  <b-col class="col-y4">
+                      <div class="circle" data-y="4" data-x="4"></div>
+                  </b-col>
+              </b-row>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Game',
+    data() {
+        return {
+            y1: ['', '', '', ''],
+            y2: ['', '', '', ''],
+            y3: ['', '', '', ''],
+            y4: ['', '', '', ''],
+        };
+    },
+
+    methods: {
+
+    },
+
+    created() {
+        this.y1 = ['', '', '', ''];
+        this.y2 = ['', '', '', ''];
+        this.y3 = ['', '', '', ''];
+        this.y4 = ['', '', '', ''];
+        alert("Listo para empezar");
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    a {
+      color: #42b983;
+    }
+
+    b-row{
+        display: block;
+    }
+
+    .circle {
+        height: 80px;
+        width: 80px;
+        border-radius: 100%;
+        background-color: slategrey;
+        display: inline-block;
+        margin: 20px;
+    }
+</style>
